8a089b350c8ed9d5e579c19962075a0ebe26d878
lanagramme <lborromee16@gmail.com>
2019-02-21 20:14:56 -0400

    .

---
+++ RPG/TPT/JS/script.js
@@ -0,0 +1,20 @@
+var perso = {name "", pv: 0};
+    
+var hro = new perso("", 10);
+var nmi = new perso("Vanir, soldat du roi demon", 10);
+
+alert(nmi.name);
+
+hro.name = prompt('bonjour vaillant Hero, quel est ton nom ? \n');
+
+alert('Donc tu est ' + hro.name + ', excellent. Malheureusement pour toi je suis ' + nmi.name + ' et je suis ici pour te vaincre!\n En garde !!');
+
+
+    alert(hro.name + ' a ' + hro.pv + 'PV restants\n' + nmi.name + ' a ' nmi.pv "PV restant\n qu'allez vous faire ?");
+
+
+while (pvperso > 0 && pvnmi > 0) {
+
+
+}
+alert("vous avez gagné !");
