67cdb6e778a6985379ddc7e6590aad746cb06d42
Lanagramme <46745901+Lanagramme@users.noreply.github.com>
2020-03-09 21:49:01 -0300

    .

---
+++ dummyitem.js
@@ -0,0 +1,110 @@
+let a = {
+	id1 : {
+		titre: "Compte rendu de la reunion du 19.02",
+		date: "19 Fevrier 2020",
+		auteur: "ecovillage",
+		tag: ["none"],
+		resume: "Pour cette première réunion nous avons établi des réunions régulières afin de garder un rythme d'avancée régulier sur le projet.",
+		contenu: `
+                <h3>Thèmes principaux autour desquels axer nos les recherches.</h3>
+                <ul>
+                    <li>Architecture (baubiologie)</li>
+                    <li>Agriculture</li>
+                    <li>hotêllerie</li>
+                    <li>Production d'électricité</li>
+                    <li>Phytoépuration</li>
+                </ul>
+
+                <h3>Architecture</h3>
+
+                <p>bois charpente</p>
+
+                <h3>Hôtellerie</h3>
+                <p>De quoi à t'on besoin dans une maison, et est-ce que cela polur ?</p>
+                <ul>
+                    <li>dormir</li>
+                    <li>stocker</li>
+                    <li>manger</li>
+                    <li>laver</li>
+                    <li>cuisiner</li>
+                </ul>
+                <br>
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Besoin\Pollution</th>
+                            <th scope="col">Production</th>
+                            <th scope="col">Consommation</th>
+                        </tr>
+                        <tbody>
+                            <tr>
+                              <th scope="row">Ammeublement</th>
+                              <td> X </td>
+                              <td> </td>
+                        </tbody>
+                    </thead>
+                </table>
+                
+
+                </table>
+                <p>Biogaz pour cuisiner</p>
+                <p>Puis de lumière</p>
+                <p>chauffage de l'eau</p>
+                <p>refroidissement de l'eau</p>
+                <p>evacuation des eaux usées</p>
+                <p>recuperation des eaux de pluie</p>
+                <p>si l'on a pas un terrain proche d'une rivière comment fait-on</p>
+                <p>refrigateur eau fraiche voir cave pour stocker</p>
+                <p>Construire les meubles de l'écovillage</p>
+
+                <h3>Phytoépuration</h3>
+                <p>Traitement dmédicaments eau ? faire des stages</p>
+                <p>faire des recherches sur le bicarbonate et le vinaigre</p>
+
+                <p></p>
+
+                <h3>Relations humaines au sein de l'écovillage</h3>
+                <p>Bien que nous choisierons nous même les individus qui participerons au projet, les tempérament humains étant changeant, un mediateur pourrait s'avérer necessaire afin de résoudre les conflits interpersonnels.</p>
+		`,
+	},
+	id2 : {
+		titre: "Compte rendu de la reunion du 20.02",
+		date: "20 Fevrier 2020",
+		auteur: "ecovillage",
+		tag: ["none"],
+		resume: "Lors de cette réunion nous avons discuté de plusieurs alternatives architecturales pour les habitations du village. Au vues de la nature de notre projet et de no possibilitées financières nous avons convenus qu'il nous fallait sortir des sentiers battus. Nous nous sommes donc tourné vers les methodes alternatives de construction autour de la planète pour de l'inspiration, et principalement dans les pays ou l'argent est un problème. \n Le mot clé de notre phylosophie étant 'penser de façon non conventionnelle'",
+		contenu: `<h3>Design explorés</h3>
+
+                <ul>
+                    <li>Maisons en dôme. <i>Article à créer</i></li>
+                    <li>Maisons en sac de sable. <i>Article à créer</i></li>
+                    <li>Maisons souterraines ou à flanc de coline. <i>Article à créer</i></li>
+                    <li>puits de lumière. <i>Article à créer</i></li>
+                    <li>toits thermorégulants. <i>Article à créer</i></li>
+                    <li>Comment creuser des puits. <i>Article à créer</i></li>
+                </ul>
+
+
+                <h3>Autres idées</h3>
+                <ul>
+                    <li>Mise au point du design des toitures végtétales. <a href="Architecture">Voir la page.</a></li>
+                    <hr>
+                    <li>Entrer en contact avec un expert pour définir nos constructions peuvent résister aux tremblements de terre et aux cyclones</li>
+                    <li>Combiner le concept de tiny house et les architectures alternatives nous permettrai de louer des chambres à la fois au nombre d'occupant et au type de logement.</li>
+                    </ul>
+
+                <h3>Conclusion</h3>
+
+                <p>A l'issue de la réunion, nous sommes arrivés à une réalisation : qu'est ce qui nous empêche de combiner différents types d'habitations au sein du village ? Pourquoi ne pas combiner différents styles architecturaux <i>au sein d'une même maison</i> ?</p>
+		`
+	},
+	id3 : {
+		titre: "0",
+		date: "0",
+		auteur: "0",
+		tag: "0",
+		resume: "0",
+		contenu: "0"
+	}
+}
